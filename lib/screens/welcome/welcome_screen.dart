d4a5bd4128a650d3247c6be571fc246b9dd8cd33
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-13 22:02:55 +0330

    install ui design package

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: Container(
+          width: 375.w,
+        ),
+      ),
+    );
+  }
+}
