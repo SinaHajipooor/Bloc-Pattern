448ae0205858d5744889de8ff1f7bdef763e2aea
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-19 20:39:14 +0330

    extract the page content into seprate widget

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -21,32 +21,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(width: 345.w, height: 345.w, child: const Text('Image one')),
-                      SizedBox(
-                        child: Text('First see Learning', style: TextStyle(color: Colors.black, fontSize: 24.sp)),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                        child: Text('Forgot about a for of paper all knowledge in one learning ! ', style: TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 14.sp)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(color: Colors.grey.withOpacity(0.1), spreadRadius: 1, blurRadius: 2, offset: const Offset(0, 1)),
-                          ],
-                        ),
-                        child: Center(child: Text('next', style: TextStyle(color: Colors.white, fontSize: 16.sp))),
-                      )
-                    ],
-                  )
+                  _page(),
+                  _page(),
+                  _page(),
+                  _page(),
                 ],
               ),
             ],
@@ -55,4 +33,33 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       ),
     );
   }
+}
+
+Widget _page() {
+  return Column(
+    children: [
+      SizedBox(width: 345.w, height: 345.w, child: const Text('Image one')),
+      SizedBox(
+        child: Text('First see Learning', style: TextStyle(color: Colors.black, fontSize: 24.sp)),
+      ),
+      Container(
+        width: 375.w,
+        padding: EdgeInsets.only(left: 30.w, right: 30.w),
+        child: Text('Forgot about a for of paper all knowledge in one learning ! ', style: TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 14.sp)),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+        width: 325.w,
+        height: 50.h,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(15.w)),
+          boxShadow: [
+            BoxShadow(color: Colors.grey.withOpacity(0.1), spreadRadius: 1, blurRadius: 2, offset: const Offset(0, 1)),
+          ],
+        ),
+        child: Center(child: Text('next', style: TextStyle(color: Colors.white, fontSize: 16.sp))),
+      )
+    ],
+  );
 }
