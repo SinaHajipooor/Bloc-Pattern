15254637fc267323ee00b83ddf4102b2e17c1afd
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-20 22:49:16 +0330

    build reusable text feild

--- lib/screens/signin/signin_screen.dart
+++ lib/screens/signin/signin_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/app/imports/app_imports.dart';
 import 'package:bloc_pattern/widgets/signin/reusable_text.dart';
 import 'package:bloc_pattern/widgets/signin/signin_app_bar.dart';
+import 'package:bloc_pattern/widgets/signin/text_feild.dart';
 import 'package:bloc_pattern/widgets/signin/third_party_login.dart';
 import 'package:flutter/material.dart';
 
@@ -23,9 +24,21 @@ class _SigninScreenState extends State<SigninScreen> {
           body: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 buildThirdPartyLogin(context),
-                reusableText('Or use your email account to login'),
+                Center(child: reusableText('Or use your email account to login')),
+                Container(
+                  margin: EdgeInsets.only(top: 66.h),
+                  padding: EdgeInsets.only(left: 25.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      reusableText('Email'),
+                      buildTextFeild('Email', 'email'),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
---
+++ lib/widgets/signin/text_feild.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_pattern/app/imports/app_imports.dart';
+
+Widget buildTextFeild(String text, String textType) {
+  return Container(
+    width: 325.w,
+    height: 50.h,
+  );
+}
