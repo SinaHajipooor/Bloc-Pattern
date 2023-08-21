09ab8b78f8e45e2db5425b9e49b019e1849552f3
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-21 14:26:57 +0330

    refactor the signin screen

--- lib/widgets/signin/third_party_login.dart
+++ lib/widgets/signin/third_party_login.dart
@@ -5,6 +5,7 @@ import 'package:bloc_pattern/widgets/signin/reusable_icon.dart';
 Widget buildThirdPartyLogin(BuildContext context) {
   return Container(
     margin: EdgeInsets.only(top: 40.h, bottom: 20.h),
+    padding: EdgeInsets.only(left: 25.w, right: 25.w),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
