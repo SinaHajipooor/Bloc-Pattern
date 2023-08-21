2f156ea63d079a48616ee83bdf3a89ee0ba0d726
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-21 13:26:10 +0330

    create a reusable custom input widget

--- lib/widgets/signin/text_feild.dart
+++ lib/widgets/signin/text_feild.dart
@@ -5,6 +5,7 @@ Widget buildTextFeild(String text, String textType) {
     width: 325.w,
     height: 50.h,
     decoration: BoxDecoration(
+      color: Colors.white,
       borderRadius: BorderRadius.all(Radius.circular(15.w)),
       border: Border.all(color: Colors.black),
     ),
@@ -17,8 +18,36 @@ Widget buildTextFeild(String text, String textType) {
           child: Image.asset('assets/icons/user.png'),
         ),
         Container(
-          width: 280.w,
+          width: 270.w,
           height: 50.h,
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            style: TextStyle(color: Colors.black, fontFamily: 'Avenir', fontSize: 14.sp),
+            decoration: InputDecoration(
+              hintText: 'Enter your email address',
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              disabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              hintStyle: TextStyle(color: Colors.grey.withOpacity(0.5)),
+            ),
+          ),
         )
       ],
     ),
