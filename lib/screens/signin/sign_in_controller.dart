7c04045d26226cef717ad48a95b78a6f06d508ed
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-22 13:35:55 +0330

    get the user credential from firebase auth

--- lib/screens/signin/sign_in_controller.dart
+++ lib/screens/signin/sign_in_controller.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/app/imports/app_imports.dart';
 import 'package:bloc_pattern/screens/signin/bloc/sign_in_blocs.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignInController {
   //feilds
@@ -7,7 +8,7 @@ class SignInController {
   // contructor
   const SignInController({required this.context});
 // methods
-  void handleSignIn(String type) {
+  void handleSignIn(String type) async {
     try {
       if (type == 'email') {
         //  the following line is just like this line : BlocProvider.of<SignInBloc>(context).state
@@ -16,6 +17,8 @@ class SignInController {
         String password = state.password;
         // validation
         if (emailAddress.isEmpty || password.isEmpty) return;
+        //  get the credential from firebase
+        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailAddress, password: password);
       }
     } catch (err) {}
   }
