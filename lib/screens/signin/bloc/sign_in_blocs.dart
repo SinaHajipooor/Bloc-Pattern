990293dd9517c86874d0089dde90354f07ce3d84
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-22 13:08:17 +0330

    implement the events handlers logic

--- lib/screens/signin/bloc/sign_in_blocs.dart
+++ lib/screens/signin/bloc/sign_in_blocs.dart
@@ -3,8 +3,18 @@ import '../../../app/imports/app_imports.dart';
 class SignInBloc extends Bloc<SignInEvents, SignInState> {
   SignInBloc() : super(const SignInState()) {
     // email
-    on<EmailEvent>((event, state) => {});
+    on<EmailEvent>(_emailEvent);
     // password
-    on<PasswordEvent>((event, state) {});
+    on<PasswordEvent>(_passwordEvent);
+  }
+
+// email event logic
+  void _emailEvent(EmailEvent event, Emitter<SignInState> emit) {
+    emit(state.copyWith(email: event.email));
+  }
+
+  // password event logic
+  void _passwordEvent(PasswordEvent event, Emitter<SignInState> emit) {
+    emit(state.copyWith(password: event.password));
   }
 }
